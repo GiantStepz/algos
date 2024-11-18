8132859ccb7b8ce50231b584cc80f95ad6971516
GiantStepz <acoop805@gmail.com>
2024-11-18 00:19:54 -0700

    Adding directory for binary tree alogs

---
+++ BinaryTree/SimilarLeafs.cpp
@@ -0,0 +1,35 @@
+// LC 872. Leaf-Similar Trees
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> leafs1;
+    vector<int> leafs2;
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        
+        traverse(root1, leafs1);
+        traverse(root2, leafs2);
+
+        return leafs1 == leafs2;
+    }
+
+    void traverse(TreeNode* root, vector<int>& leafs) {
+        if(root == nullptr) return;
+
+        traverse(root->left, leafs);
+        if(root->left == nullptr && root->right == nullptr) {
+            leafs.push_back(root->val);
+        }
+        traverse(root->right, leafs);
+    }
+};
